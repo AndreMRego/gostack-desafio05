356f7b29f1264f6002c0b4030ce02a5bf21ddd77
Andre Rego <andremregodev@gmail.com>
2019-09-15 21:11:18 -0300

    Buscando repositórios da api do GitHub e listando

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
 
-import { Title } from './styles'
+import api from '../../services/api'
 
-function Main() {
-  return <Title>Hello World</Title>
+import { SubmitButton, Container, Form, List } from './styles'
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault()
+
+    this.setState({ loading: true })
+    console.log(this.state.newRepo)
+    const { newRepo, repositories } = this.state
+    const response = await api.get(`repos/${newRepo}`)
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    })
+
+    console.log(response)
+  }
+
+  render() {
+    const { newRepo, repositories, loading } = this.state
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(respository => (
+            <li key={respository.name}>
+              <span>{respository.name}</span>
+              <a href="">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    )
+  }
 }
-
-export default Main
